47e7ee8b759fa7c3c1f235182a54ddca8b1a1b1c
AnujMadhoria <166337358+AnujMadhoria@users.noreply.github.com>
2025-06-12 17:13:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Or your frontend URL
+    origin: 'https://intvfrontendvercel.vercel.app/', // Or your frontend URL
     methods: ['GET', 'POST','PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true
   },
