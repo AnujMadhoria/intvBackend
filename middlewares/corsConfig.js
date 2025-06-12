5de014bea9ab5462065a54338fcdbacf0bb224b9
AnujMadhoria <166337358+AnujMadhoria@users.noreply.github.com>
2025-06-12 17:09:54 +0530

    Update corsConfig.js

--- middlewares/corsConfig.js
+++ middlewares/corsConfig.js
@@ -3,7 +3,7 @@
 const cors = require('cors');
 
 const corsConfig = cors({
-  origin: 'http://localhost:5173', // your frontend URL
+  origin: 'intvfrontendvercel.vercel.app', // your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'], // optional, for extra clarity
 });
